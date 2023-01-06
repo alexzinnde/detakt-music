7df044380854fe101b0faf34371c8e5db507c5e1
Alexander Zinn <61348227+alexzinnde@users.noreply.github.com>
2023-01-06 00:34:59 -0500

    delete root tsconfig

--- app/frontend-semantic/src/components/ContentContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Container} from 'semantic-ui-react'
-
-//@ts-ignore
-const ContentContainer = ({children}) => {
-  return <Container style={{marginTop: '70px'}}>{children}</Container>
-}
-
-export default ContentContainer
